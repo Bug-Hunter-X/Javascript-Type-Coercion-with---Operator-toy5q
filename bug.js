017542133039ce4e51dc2f12ebb6f28d5c9deb23
BugHunter-X <askzenai@gmail.com>
2024-12-26 12:08:03 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,2 @@
+function foo(a,b){return a+b;}console.log(foo(1, "2")); //Output: 12
+This is because in javascript when you use the + operator between a number and a string, javascript converts the number to a string and then concatenates the two strings.
